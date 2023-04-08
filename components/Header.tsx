538861ca05a426b3c99ab2d4db4e6f9b2902214e
BisiDavi <oludavidconnect@gmail.com>
2023-04-08 17:04:41 +0100

    sharp module added

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import Logo from "@/components/Logo";
 
 export default function Header() {
   return (
-    <header className="py-5 border-b">
+    <header className="py-6 border-b">
       <nav className="container">
         <Logo />
       </nav>
--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,8 +2,27 @@ import Link from "next/link";
 
 export default function Logo() {
   return (
-    <Link href="/" className="border-2 rounded-l-lg  p-2">
-      Convert<span className="bg-orange-700 text-white p-1 rounded-r-lg">Image</span>
-    </Link>
+    <div className="logo">
+      <Link href="/" className="flex items-center">
+        <span className="text p-2 m-0 rounded-l-lg border-r-0 border">
+          Convert
+        </span>
+        <span className="colored bg-blue-800 text-white p-2 border border-l-0 rounded-r-lg ">
+          Image
+        </span>
+      </Link>
+      <style jsx>
+        {`
+          .logo:hover span.text {
+            background-color: #1e40af;
+            color: white;
+          }
+          .logo:hover span.colored {
+            background-color: white;
+            color: black;
+          }
+        `}
+      </style>
+    </div>
   );
 }
