7c57f5bb6e2ace19532d188f33659b2f6c12ae66
BisiDavi <oludavidconnect@gmail.com>
2023-04-10 12:43:03 +0100

    UploadImage updated

--- components/UploadImage.tsx
+++ components/UploadImage.tsx
@@ -1,8 +1,18 @@
+import { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
 
 export default function UploadImage() {
+  const onDrop = useCallback((acceptedFiles) => {
+    // Do something with the files
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+  const text = isDragActive
+    ? "Drop the files here"
+    : "Drag 'n' drop some files here, or click to select files";
   return (
-    <div className="border rounded-lg h-96 w-2/5">
-
+    <div className="border rounded-lg h-96 w-2/5 p-4 flex " {...getRootProps()}>
+      <input {...getInputProps()} />
+      <p className="text-center mx-auto flex justify-center items-center">{text}</p>
     </div>
-  )
+  );
 }
