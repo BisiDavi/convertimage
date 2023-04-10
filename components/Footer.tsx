6aa27007421c6ebb2f06344feb7fb3dbb585c189
BisiDavi <oludavidconnect@gmail.com>
2023-04-10 10:45:42 +0100

    footer updated

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,7 @@
 export default function Footer() {
   return (
-    <footer>Footer</footer>
+    <footer className="bg-black  text-white text-center py-8">
+        Made with ❤️ and 💧
+    </footer>
   )
 }
