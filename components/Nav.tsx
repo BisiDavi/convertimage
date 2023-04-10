e5cb58c93bcd76a339c8ba9ca5a368e92449835e
BisiDavi <oludavidconnect@gmail.com>
2023-04-10 13:08:15 +0100

    nav updated

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,8 +2,11 @@ export default function Nav() {
   return (
     <nav>
       <ul className="flex space-x-6 p-0">
-        <li>JPG to PNG</li>
-        <li>PNG to WEBP</li>
+        <li>Convert Image</li>
+        <li>Image Editor</li>
+        <li className="flex relative">
+          Video Editor <span className="absolute  -right-16 -top-2 bg-orange-300 text-xs rounded-lg px-1 py-0">Coming Soon</span>
+        </li>
       </ul>
     </nav>
   );
