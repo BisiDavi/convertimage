89136c95ffbe695ea4a5b008531263a2f5fcaebc
BisiDavi <oludavidconnect@gmail.com>
2023-04-10 14:04:10 +0100

    uploadImage updated

--- components/ImageThumbs.tsx
+++ components/ImageThumbs.tsx
@@ -6,17 +6,19 @@ interface Props {
 }
 
 export default function ImageThumbs({ files }: Props) {
+  console.log("files", files);
   useEffect(() => {
     return () => files.forEach((file) => URL.revokeObjectURL(file.preview()));
   }, []);
+
   return (
-    <div className="thumbs">
+    <div className="thumbs flex space-x-8 my-4 border p-4 rounded-lg">
       {files.length > 0 &&
         files.map((file) => (
           <div className="thumb" key={file.name}>
             <img
               src={file.preview}
-              onLoad={() => URL.revokeObjectURL(file.preview)}
+              onLoad={() => URL.revokeObjectURL(file?.preview)}
               alt={file.name}
             />
           </div>
--- components/UploadImage.tsx
+++ components/UploadImage.tsx
@@ -1,5 +1,9 @@
 import useUploadImage from "@/hooks/useUploadImage";
-import ImageThumbs from "@/components/ImageThumbs";
+import dynamic from "next/dynamic";
+
+const ImageThumbs = dynamic(() => import("@/components/ImageThumbs"), {
+  ssr: false,
+});
 
 export default function UploadImage() {
   const { files, isDragActive, getRootProps, getInputProps } = useUploadImage();
@@ -9,9 +13,9 @@ export default function UploadImage() {
     : "Drag 'n' drop some files here, or click to select files";
 
   return (
-    <>
+    <div className="flex flex-col w-1/2">
       <div
-        className="border-4 transition duration-75 ease-in-out border-dotted rounded-lg h-96 w-2/5 p-4 flex "
+        className="border-4 transition duration-75 ease-in-out border-dotted  h-96 w-4/5 mx-auto rounded-lg p-4 flex "
         {...getRootProps()}
       >
         <input
@@ -23,7 +27,7 @@ export default function UploadImage() {
           {text}
         </p>
       </div>
-      <ImageThumbs files={files} />
-    </>
+      {files.length > 0 && <ImageThumbs files={files} />}
+    </div>
   );
 }
