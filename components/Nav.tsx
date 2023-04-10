7853248a428d2a6558ead00b1a3ebc3c073e8247
BisiDavi <oludavidconnect@gmail.com>
2023-04-10 13:17:02 +0100

    nav.json added

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,12 +1,29 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+import navContent from "@/json/nav.json";
+
 export default function Nav() {
+  const router = useRouter();
   return (
     <nav>
       <ul className="flex space-x-6 p-0">
-        <li>Convert Image</li>
-        <li>Image Editor</li>
-        <li className="flex relative">
-          Video Editor <span className="absolute  -right-16 -top-2 bg-orange-300 text-xs rounded-lg px-1 py-0">Coming Soon</span>
-        </li>
+        {navContent.map((navItem) => {
+          const activeLink =
+            router.route === navItem.link ? "text-orange-300" : "";
+          return (
+            <li key={navItem.link}>
+              <Link href="/" className={`relative ${activeLink}`}>
+                {navItem.text}
+                {navItem.text === "Video Editor" && (
+                  <span className="absolute -right-16 -top-4 bg-orange-300 text-xs rounded-lg px-1 py-0.5">
+                    Coming Soon
+                  </span>
+                )}
+              </Link>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
