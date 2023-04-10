43b6bab3dcf2c869f72a565c24551454a005bfe7
BisiDavi <oludavidconnect@gmail.com>
2023-04-10 11:26:18 +0100

    typwwriter module added

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,8 +3,12 @@ import Logo from "@/components/Logo";
 export default function Header() {
   return (
     <header className="py-6 border-b">
-      <nav className="container">
+      <nav className="container flex justify-between">
         <Logo />
+        <ul className="flex space-x-6">
+          <li>JPG to PNG</li>
+          <li>PNG to WEBP</li>
+        </ul>
       </nav>
     </header>
   );
