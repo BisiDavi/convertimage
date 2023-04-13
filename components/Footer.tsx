3c01a89a7e6970848040bce0b04b9b027eb7e2af
BisiDavi <oludavidconnect@gmail.com>
2023-04-13 14:20:53 +0100

    footer updated

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-gray-200 fixed w-screen bottom-0 text-black text-center py-4">
-        Made with ❤️ and ⚡️
+      Made with ❤️ by <a href="https://olubisi-david.vercel.app" target="_blank" className="font-bold text-blue-500 hover:text-red-500">Dave</a>
     </footer>
-  )
+  );
 }
